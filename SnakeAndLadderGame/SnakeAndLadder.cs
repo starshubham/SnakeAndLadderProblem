f2ed55a4e45baec03bf2ee4b4761562d1ebd3ac2
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-30 23:31:39 +0530

    [add] Shubham added SnakeAndLadder program

---
+++ SnakeAndLadderGame/SnakeAndLadder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    /* UC1:- Snake and Ladder game played with single player at start position 0
+     */
+    public class SnakeAndLadder
+    {
+        int START_POSITION = 0;
+        public void PlayGame()
+        {
+            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);
+            Console.ReadLine();
+        }
+    }
+}
