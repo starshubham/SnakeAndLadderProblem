70807540960c0a9665d545d215cd5ec640ffebbf
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-31 08:39:10 +0530

    [add] Shubham added UC2 of SnakeAndLadder program

---
+++ SnakeAndLadderGame/RollDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    public class RollDice
+    {
+        public int rollDice()
+        {
+            Random random = new Random();
+            int dice = random.Next(1, 7);     //To Get random number between 1 to 6
+            return dice;
+        }
+    }
+}
