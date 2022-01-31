49b9100421f7d3c4dd89e0400d8b693d87193892
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-31 09:05:47 +0530

    [add] Shubham added UC4 of SnakeAndLadder program

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -21,20 +21,45 @@ namespace SnakeAndLadderGame
             //Console.WriteLine("Roll Dice: " + playerDice);
 
             //UC3
-            RollDice roll = new RollDice();   //Creating an object of RollDice class.   
-            int playerDice = roll.rollDice();
-            int option = roll.checkOption();
-            switch (option)
+            //RollDice roll = new RollDice();   //Creating an object of RollDice class.   
+            //int playerDice = roll.rollDice();
+            //int option = roll.checkOption();
+            //switch (option)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Player Got Ladder.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Player got Snake.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Player not playing.");
+            //        break;
+            //}
+
+            /* UC4:- Repeat till the Player reaches the winning position 100. 
+               - Note In case the player position moves below 0, then the player restarts from 0
+             */
+            int playerPosition = 0;
+            RollDice roll = new RollDice();   //Creating an object of RollDice class. 
+            while (playerPosition != 100)
             {
-                case 1:
-                    Console.WriteLine("Player Got Ladder.");
-                    break;
-                case 2:
-                    Console.WriteLine("Player got Snake.");
-                    break;
-                default:
-                    Console.WriteLine("Player not playing.");
-                    break;
+                int playerDice = roll.rollDice();
+                int option = roll.checkOption();
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine($"Player Got Ladder and Player Position = {playerPosition}");
+                        playerPosition += playerDice;
+                        break;
+                    case 2:
+                        Console.WriteLine($"Player got Snake and Player Position = {playerPosition}");
+                        playerPosition -= playerDice;
+                        break;
+                    default:
+                        Console.WriteLine($"Player not playing and Player Position = {playerPosition}");
+                        break;
+                }
             }
         }
     }
