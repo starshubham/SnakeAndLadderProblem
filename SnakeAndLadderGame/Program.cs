0a70a762633f5d9da4e1d1818c71f2349a68b995
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-31 08:51:36 +0530

    [add] Shubham added UC3 of SnakeAndLadder program

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -16,10 +16,26 @@ namespace SnakeAndLadderGame
             /* UC2:- The Player rolls the die to get a number between 1 to 6. 
              * - Use ((RANDOM)) to get the number between */
 
-            RollDice roll = new RollDice();   //Creating an object of RollDice class.
-            
-            int playerDice = roll.rollDice();  // Calling Method of RollDice class.
-            Console.WriteLine("Roll Dice: " + playerDice);
+            //RollDice roll = new RollDice();   //Creating an object of RollDice class.           
+            //int playerDice = roll.rollDice();  // Calling Method of RollDice class.
+            //Console.WriteLine("Roll Dice: " + playerDice);
+
+            //UC3
+            RollDice roll = new RollDice();   //Creating an object of RollDice class.   
+            int playerDice = roll.rollDice();
+            int option = roll.checkOption();
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Player Got Ladder.");
+                    break;
+                case 2:
+                    Console.WriteLine("Player got Snake.");
+                    break;
+                default:
+                    Console.WriteLine("Player not playing.");
+                    break;
+            }
         }
     }
 }
