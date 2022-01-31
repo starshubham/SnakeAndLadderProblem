511c0b71b2ac3d836d13d97862bd30ac7fb10075
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-31 09:14:11 +0530

    [add] Shubham Modified UC4 of SnakeAndLadder program

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -5,9 +5,16 @@ namespace SnakeAndLadderGame
     
     class Program
     {
+        // From UC4
+        //Constants 
+        public const int NO_PLAY = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+        public const int FINAL = 100;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("***Welcome To Snake and Ladder Gmae***");
+            Console.WriteLine("***Welcome To Snake and Ladder Game***");
 
             /* UC1:- Snake and Ladder game played with single player at start position 0 */
             //int playerPosition = 0;
@@ -40,27 +47,45 @@ namespace SnakeAndLadderGame
             /* UC4:- Repeat till the Player reaches the winning position 100. 
                - Note In case the player position moves below 0, then the player restarts from 0
              */
-            int playerPosition = 0;
-            RollDice roll = new RollDice();   //Creating an object of RollDice class. 
-            while (playerPosition != 100)
+
+            Console.WriteLine();
+            Console.WriteLine("Single Player mode: Starting position 0");
+            Console.WriteLine("GAME START");
+
+            //Variables
+            int position = 0;
+
+            //creatting random object
+            Random die = new Random();
+            Random options = new Random();
+
+            while (position <= FINAL)
             {
-                int playerDice = roll.rollDice();
-                int option = roll.checkOption();
-                switch (option)
+                int dice = die.Next(1, 7);
+                Console.WriteLine("The number on this die roll is: " + dice);
+                int opt = options.Next(0, 3);
+
+                if (opt == NO_PLAY)
                 {
-                    case 1:
-                        Console.WriteLine($"Player Got Ladder and Player Position = {playerPosition}");
-                        playerPosition += playerDice;
-                        break;
-                    case 2:
-                        Console.WriteLine($"Player got Snake and Player Position = {playerPosition}");
-                        playerPosition -= playerDice;
-                        break;
-                    default:
-                        Console.WriteLine($"Player not playing and Player Position = {playerPosition}");
-                        break;
+                    Console.WriteLine("No play: Player in same position-- " + position);
+                }
+                else if (opt == LADDER)
+                {
+                    position = position + dice;
+                    Console.WriteLine("Ladder!!! new postion-- " + position);
+                }
+                else
+                {
+                    position = position - dice;
+                    Console.WriteLine("Snake!!! new position-- " + position);
+                }
+
+                if (position < 0)
+                {
+                    position = 0;
                 }
             }
+
         }
     }
 }
